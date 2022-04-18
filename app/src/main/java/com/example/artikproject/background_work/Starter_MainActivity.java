bc1d9c055f2a7e6e79a35e0f147d3848305de808
Artem <Krutoypan3@gmail.com>
2022-04-18 16:42:41 +0300

    Add loading anim

---
+++ app/src/main/java/com/example/artikproject/background_work/Starter_MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.artikproject.background_work;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.artikproject.R;
+import com.example.artikproject.background_work.CheckInternetConnection;
+import com.example.artikproject.background_work.CustomAlertDialog;
+import com.example.artikproject.background_work.adapters.ListViewAdapter;
+import com.example.artikproject.background_work.site_parse.GetRasp;
+import com.example.artikproject.background_work.site_parse.GetFullGroupList_Online;
+import com.example.artikproject.layout.MainActivity;
+import com.example.artikproject.layout.Raspisanie_show;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Starter_MainActivity extends Thread {
+    Activity act;
+    ImageView loading_ico;
+
+    /**
+     * Показывает список всех факультетов и их групп
+     * @param act Активити
+     * @param v View
+     */
+    public Starter_MainActivity(Activity act, ImageView v){
+        this.act = act;
+        loading_ico = v;
+    }
+
+    @Override
+    public void run() {
+        try{
+            Animation animRotate = AnimationUtils.loadAnimation(act.getApplicationContext(), R.anim.scale_long);
+            loading_ico.startAnimation(animRotate);
+            try {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(act.getApplicationContext(), MainActivity.class);
+            act.startActivity(intent);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/artikproject/layout/StartActivity.java
@@ -0,0 +1,23 @@
+package com.example.artikproject.layout;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.artikproject.R;
+import com.example.artikproject.background_work.Starter_MainActivity;
+import com.example.artikproject.background_work.datebase.DataBase_Local;
+import com.example.artikproject.background_work.theme.Theme;
+
+public class StartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MainActivity.sqLiteDatabase = new DataBase_Local(getApplicationContext()).getWritableDatabase();
+        Theme.setting();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+        ImageView loading_ico = findViewById(R.id.loading_ico);
+        new Starter_MainActivity(this, loading_ico).start();
+    }
+}
