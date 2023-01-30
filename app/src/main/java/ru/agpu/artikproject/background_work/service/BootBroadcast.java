dd4625b5029e8b2d1851efef08b9b01e6834eb31
Artem Oganesyan <Krutoypan3@gmail.com>
2023-01-31 02:35:58 +0300

    Исправил краш из-за фонового сервиса

--- app/src/main/java/ru/agpu/artikproject/background_work/service/BootBroadcast.java
+++ app/src/main/java/ru/agpu/artikproject/background_work/service/BootBroadcast.java
@@ -3,11 +3,16 @@ package ru.agpu.artikproject.background_work.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class BootBroadcast extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        context.startService(new Intent(context, PlayService.class));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(new Intent(context, PlayService.class));
+        } else {
+            context.startService(new Intent(context, PlayService.class));
+        }
     }
 }
